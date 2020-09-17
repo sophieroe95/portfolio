d1653eb02deee88f729cf8dbcb9129ed720bb9a1
Sophie Roe <sophie.roe@Nology.local>
2020-09-17 17:31:31 +0100

    portfolio files added

---
+++ main.js
@@ -0,0 +1,8 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+// added using the toggle with an active class to avoid 'if'
+toggleButton.addEventListener('click', () => {
+  // get navbar - access all the different classes on it - toggle active class
+  navbarLinks.classList.toggle('active');
+})
